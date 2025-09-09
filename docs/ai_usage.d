1a2ff03a540dda2ed8b495a34ee8af1201cfc99e
MAlam1802 <67410387+MAlam1802@users.noreply.github.com>
2025-09-08 22:50:48 -0500

    Update ai_usage.d

--- docs/ai_usage.d
+++ docs/ai_usage.d
@@ -4,3 +4,6 @@
 - Snippet of prompt(s):
 - What I changed before committing:
 - How I verified correctness (tests, sample data):
+- Gemini in Google Colab
+  Prompt "How to calculate summation of numbers"
+  
